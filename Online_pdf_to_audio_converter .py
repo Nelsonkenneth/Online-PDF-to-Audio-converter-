bf94c970e618cb3b92e73247f177498db7dd9583
Kenneth <nelsonkenneth32@gmail.com>
2023-02-13 21:20:36 +0100

    just modified it

--- Online_pdf_to_audio_converter .py
+++ Online_pdf_to_audio_converter .py
@@ -7,9 +7,9 @@ import os
 from io import BytesIO
 
 col1, col2, col3 = st.columns(3)
-col1.metric("Temperature", "9 °C", "1 °C")
+col1.metric("Temperature", "9 °C", "1 °C")# just to see if it works 
 col2.metric("Wind", "9 mph", "-8%")
-col3.metric("Humidity", "86%", "4%")
+col3.metric("Humidity", "86%", "4%")# if not ahh
 
 st.title("PDF to Audio Converter")
 
@@ -18,7 +18,7 @@ for uploaded_file in uploaded_files:
     bytes_data = uploaded_file.read()
     st.write("filename:", uploaded_file.name)
 
-    pdf_file = BytesIO(bytes_data)
+    pdf_file = BytesIO(bytes_data) 
     pdffile = PyPDF2.PdfFileReader(pdf_file)
     no_of_pages = pdffile.getNumPages()
 
